ef16aa9a5cee87697453fcd5c757d7501dc6a0c1
MaximeFerret <maximeferret3@gmail.com>
2025-03-08 18:34:35 +0100

    ajout de docstrings et correction des erreurs de linters

--- app/backend/business_object/user.py
+++ app/backend/business_object/user.py
@@ -6,6 +6,21 @@ bcrypt = Bcrypt()
 
 
 class User(db.Model, UserMixin):
+    """
+    Classe représentant un utilisateur.
+
+    Cette classe représente un utilisateur avec son nom d'utilisateur,
+    son email, son mot de passe et si c'est un hôte ou non.
+
+    Attributes:
+    -----------
+        id (int): l'identifiant de l'utilisateur.
+        username (str): le nom d'utilisateur de l'utilisateur.
+        email (str): l'email de l'utilisateur.
+        password_hash (str): le mot de passe de l'utilisateur.
+        is_host (bool): si l'utilisateur est un hôte ou non.
+        quizzes (list): les quizzes créés par l'utilisateur.
+    """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
@@ -14,8 +29,18 @@ class User(db.Model, UserMixin):
     quizzes = db.relationship('Quiz', backref='creator', lazy=True)
 
     def set_password(self, password):
+        """
+        Définit le mot de passe de l'utilisateur.
+
+        Cette méthode permet de définir le mot de passe de l'utilisateur.
+        """
         hashed = bcrypt.generate_password_hash(password)
         self.password_hash = hashed.decode("utf-8")
 
     def check_password(self, password):
+        """
+        Vérifie si le mot de passe est correct.
+
+        Cette méthode permet de vérifier si le mot de passe est correct.
+        """
         return bcrypt.check_password_hash(self.password_hash, password)
--- app/backend/service/user_service.py
+++ app/backend/service/user_service.py
@@ -1,43 +1,89 @@
 from datetime import datetime, timedelta, timezone
-
 from backend.dao.user_dao import UserDAO
-from flask_login import login_user
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
 
 
 class UserService:
+    """
+    Service pour les opérations liées aux utilisateurs.
+    """
     @staticmethod
     def create_user(username, email, password, is_host):
-        """Création de l'utilisateur et sauvgarder"""
+        """Création de l'utilisateur et sauvgarder
+
+        Cette méthode permet de créer un utilisateur et de le sauvegarder
+        dans la base de données.
+
+        Parameters:
+        -----------
+            username (str): le nom d'utilisateur de l'utilisateur.
+            email (str): l'email de l'utilisateur.
+            password (str): le mot de passe de l'utilisateur.
+            is_host (bool): si l'utilisateur est un hôte ou non.
+
+        Return:
+        -------
+            User: l'utilisateur créé.
+        """
         password_hash = bcrypt.generate_password_hash(password)
-        new_user = UserDAO.create_user(username, email, password_hash, is_host)
-        return new_user
+        return UserDAO.create_user(username, email, password_hash, is_host)
 
     @staticmethod
     def authenticate_user(email, password):
-        """L'authentification de l'utilisateur"""
+        """Authentification de l'utilisateur
+
+        Cette méthode permet d'authentifier un utilisateur en vérifiant
+        son email et son mot de passe.
+
+        Parameters:
+        -----------
+            email (str): l'email de l'utilisateur.
+            password (str): le mot de passe de l'utilisateur.
+        """
         user = UserDAO.get_user_by_email(email)
 
-        if not user or not bcrypt.check_password_hash(user.password_hash, password):
+        if not user or not bcrypt.check_password_hash(user.password_hash,
+                                                      password):
             return None
 
         return user
-    
+
     @staticmethod
     def get_user_by_email(email):
-        """Trouver l'utilisateur via email"""
+        """Trouver l'utilisateur via email
+
+        Cette méthode permet de trouver un utilisateur via son email.
+
+        Parameters:
+        -----------
+            email (str): l'email de l'utilisateur.
+        """
         return UserDAO.get_user_by_email(email)
-    
+
     @staticmethod
     def get_user_by_username(id):
-        """Trouver l'utilisateur via identifiant"""
+        """Trouver l'utilisateur via identifiant
+
+        Cette méthode permet de trouver un utilisateur via son identifiant.
+
+        Parameters:
+        -----------
+            id (int): l'identifiant de l'utilisateur.
+        """
         return UserDAO.get_user_by_username(id)
 
     @staticmethod
     def set_user_session(user_id):
-        """Configurer la session de l'utilisateur"""
+        """Configurer la session de l'utilisateur
+
+        Cette méthode permet de configurer la session de l'utilisateur.
+
+        Parameters:
+        -----------
+            user_id (int): l'identifiant de l'utilisateur.
+        """
         return {
             "is_user": True,
             "user_last_active": datetime.now(timezone.utc),
@@ -46,22 +92,59 @@ class UserService:
 
     @staticmethod
     def set_admin_session():
-        """Configurer la session de l'admin"""
-        return {"is_admin": True, "admin_last_active": datetime.now(timezone.utc)}
+        """Configurer la session de l'admin
+
+        Cette méthode permet de configurer la session de l'admin.
+
+        Return:
+        -------
+            dict: la session de l'admin.
+        """
+        return {"is_admin": True,
+                "admin_last_active": datetime.now(timezone.utc)}
 
     @staticmethod
     def reset_user_session():
-        """Réinitialiser la session de l'utilisateur"""
+        """Réinitialiser la session de l'utilisateur
+
+        Cette méthode permet de réinitialiser la session de l'utilisateur.
+
+        Return:
+        -------
+            dict: la session de l'utilisateur.
+        """
         return {"is_user": None, "user_last_active": None}
 
     @staticmethod
     def reset_admin_session():
-        """Réinitialiser la session de l'admin"""
+        """Réinitialiser la session de l'admin
+
+        Cette méthode permet de réinitialiser la session de l'admin.
+
+        Return:
+        -------
+            dict: la session de l'admin.
+        """
         return {"is_admin": None, "admin_last_active": None}
 
     @staticmethod
     def check_session_expiry(last_active, timeout=10):
-        """Vérifier si la session est expirée"""
+        """Vérifier si la session est expirée
+
+        Cette méthode permet de vérifier si la session est expirée.
+
+        Parameters:
+        -----------
+            last_active (datetime): la date de dernière activité de
+                l'utilisateur.
+            timeout (int): le temps de déconnexion de l'utilisateur. Par
+                défaut, il est de 10 minutes.
+
+        Return:
+        -------
+            bool: True si la session est expirée, False sinon.
+        """
         if not last_active:
             return True
-        return datetime.now(timezone.utc) - last_active > timedelta(minutes=timeout)
+        now = datetime.now(timezone.utc)
+        return now - last_active > timedelta(minutes=timeout)
