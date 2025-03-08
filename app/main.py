1d6a4f94492f82aefdbed8918a1af74da9763283
MaximeFerret <maximeferret3@gmail.com>
2025-03-08 20:47:49 +0100

    correction linter main

--- app/main.py
+++ app/main.py
@@ -1,17 +1,14 @@
-import logging
 import os
 
 from backend.business_object.db import db
 from backend.business_object.user import User
 from backend.controller.quiz_ctrl import quiz_bp
-# from flask_wtf.csrf import CSRFProtect
 from backend.controller.user_ctrl import auth_bp
 from backend.utils.socketio_events import socketio
 from dotenv import load_dotenv
 from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_socketio import SocketIO
 
 dotenv_path = "/home/ensai/BuzzerQuiz/app/.env/.env"
 load_dotenv(dotenv_path=dotenv_path)
@@ -39,16 +36,10 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "authentication.login"
 
-# csrf = CSRFProtect(app)
-# socketio = SocketIO(app, cors_allowed_origins="*")
-
-# app.permanent_session_lifetime = app.config['PERMANENT_SESSION_LIFETIME']
-
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
 
 
 @app.route("/")
@@ -59,7 +50,6 @@ def homepage():
 if __name__ == "__main__":
     with app.app_context():
         # print("Flask URL Map:", app.url_map)
-        # print("Flask URL Map:", app.url_map)  # 打印所有可用路由
 
         db.create_all()
 
