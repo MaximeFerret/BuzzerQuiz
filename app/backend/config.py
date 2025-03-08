f97fe58524d3d87c12c524eb32fe89e280915d5a
hiba <hiba.karim@eleve.ensai.fr>
2025-03-08 07:24:35 +0100

    Variables environnements

--- app/backend/config.py
+++ app/backend/config.py
@@ -1,6 +1,10 @@
+import os
+
+
 class Config:
-    SECRET_KEY = "EnsaiBruz35170!"  # Change cette clé pour plus de sécurité
-    SQLALCHEMY_DATABASE_URI = "sqlite:///buzzerquiz.db"  # Base de données SQLite
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "SQLALCHEMY_DATABASE_URI", "sqlite:///buzzerquiz.db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ADMIN_SECRET_CODE = "admin123"
-    # PERMANENT_SESSION_LIFETIME = 15 * 60
+    ADMIN_SECRET_CODE = os.getenv("ADMIN_SECRET_CODE", "admin123")
--- app/main.py
+++ app/main.py
@@ -1,13 +1,27 @@
+import logging
+import os
+
 from backend.business_object.db import db
 from backend.business_object.user import User
+from backend.controller.quiz_ctrl import quiz_bp
+# from flask_wtf.csrf import CSRFProtect
+from backend.controller.user_ctrl import auth_bp
+from backend.utils.socketio_events import socketio
+from dotenv import load_dotenv
 from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_socketio import SocketIO
-#from flask_wtf.csrf import CSRFProtect
-from backend.controller.user_ctrl import auth_bp
-from backend.controller.quiz_ctrl import quiz_bp
-from backend.utils.socketio_events import socketio
+
+dotenv_path = "/home/ensai/BuzzerQuiz/app/.env/.env"
+load_dotenv(dotenv_path=dotenv_path)
+
+SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
+DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///buzzerquiz.db")
+
+# Affichage des variables pour vérifier si elles sont bien chargées
+print(f"SECRET_KEY: {SECRET_KEY}")
+print(f"DATABASE URI: {DATABASE_URI}")
 
 
 app = Flask(__name__, template_folder="frontend/templates")
@@ -25,8 +39,8 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "authentication.login"
 
-#csrf = CSRFProtect(app)
-#socketio = SocketIO(app, cors_allowed_origins="*")
+# csrf = CSRFProtect(app)
+# socketio = SocketIO(app, cors_allowed_origins="*")
 
 # app.permanent_session_lifetime = app.config['PERMANENT_SESSION_LIFETIME']
 
@@ -35,6 +49,7 @@ login_manager.login_view = "authentication.login"
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 @app.route("/")
 def homepage():
     return render_template("index.html")
@@ -42,7 +57,7 @@ def homepage():
 
 if __name__ == "__main__":
     with app.app_context():
-        #print("Flask URL Map:", app.url_map)  # 打印所有可用路由
+        # print("Flask URL Map:", app.url_map)  # 打印所有可用路由
 
         db.create_all()  # Crée la base de données si elle n'existe pas encore
     socketio.run(app, host="0.0.0.0", debug=True)
