f9459d411512ffc09ad2e22d1a3603fbc9f4db29
MaximeFerret <maximeferret3@gmail.com>
2025-03-08 17:22:40 +0100

    création des fichiers __init__ manquants

--- app/main.py
+++ app/main.py
@@ -3,8 +3,6 @@ from backend.business_object.user import User
 from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_socketio import SocketIO
-#from flask_wtf.csrf import CSRFProtect
 from backend.controller.user_ctrl import auth_bp
 from backend.controller.quiz_ctrl import quiz_bp
 from backend.utils.socketio_events import socketio
@@ -25,15 +23,11 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "authentication.login"
 
-#csrf = CSRFProtect(app)
-#socketio = SocketIO(app, cors_allowed_origins="*")
-
-# app.permanent_session_lifetime = app.config['PERMANENT_SESSION_LIFETIME']
-
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 @app.route("/")
 def homepage():
@@ -42,17 +36,17 @@ def homepage():
 
 if __name__ == "__main__":
     with app.app_context():
-        #print("Flask URL Map:", app.url_map)
+        # print("Flask URL Map:", app.url_map)
 
         db.create_all()
 
-    #import socket
+    import socket
 
     # pour avoir l'ip locale si elle ne s'affiche pas ou si elle est 127.0.0.1
-    #def get_local_ip():
-    #    return socket.gethostbyname(socket.gethostname())
+    def get_local_ip():
+        return socket.gethostbyname(socket.gethostname())
 
-    #local_ip = get_local_ip()
-    #print(f"Accède à l'application sur : http://{local_ip}:5000")
+    local_ip = get_local_ip()
+    print(f"Accède à l'application sur : http://{local_ip}:5000")
 
     socketio.run(app, host="0.0.0.0", debug=True)
